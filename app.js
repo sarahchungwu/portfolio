743f1859996e042e7b14492237207cb365acb0cf
Sarah Zhong <sarahchungwu@gmail.com>
2023-07-11 18:07:34 +1200

    created the box div in the boxContainer by using Javascript

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('its work!!')
+
+const boxContainer = document.querySelector('.boxContainer')
+
+for (let i = 0; i < 365; i++) {
+  const el = document.createElement('div')
+  el.className = 'box'
+  boxContainer.appendChild(el)
+}
